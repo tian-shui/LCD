fd468297e080ecab72ebf272d4ff2a44d98979c0
tian shui <794064979@qq.com>
2019-05-30 08:26:05 +0800

    add

--- main/main.js
+++ main/main.js
@@ -5,44 +5,44 @@ module.exports = function main(x) {
 	out_str3 = '';
 	for (i = 0; i < str.length;i++){
 	  if (str[i] == 1) {
-		out_str1 += '...';
+		out_str1 += '... ';
 	  } else if (str[i] == 4) {
-		out_str1 += '...';
+		out_str1 += '... ';
 	  } else {
-		out_str1 += '._.';
+		out_str1 += '._. ';
 	  }
 	  
 	  if (str[i] == 0) {
-		out_str2 += '|.|';
+		out_str2 += '|.| ';
 	  }else if (str[i] == 1){
-		out_str2 += '..|';
+		out_str2 += '..| ';
 	  }else if (str[i] == 7){
-		out_str2 += '..|';
+		out_str2 += '..| ';
 	  }else if (str[i] == 2){
-		out_str2 += '._|';
+		out_str2 += '._| ';
 	  }else if (str[i] == 3){
-		out_str2 += '._|';
+		out_str2 += '._| ';
 	  }else if (str[i] == 5){
-		out_str2 += '|_.';
+		out_str2 += '|_. ';
 	  }else if (str[i] == 6){
-		out_str2 += '|_.';
+		out_str2 += '|_. ';
 	  } else {
-		out_str2 += '|_|  ';
+		out_str2 += '|_| ';
 	  }		
 	  if (str[i] == 6){
-		out_str3 += '|_|';
+		out_str3 += '|_| ';
 	  }else if (str[i] == 8){
-		out_str3 += '|_|';		  
+		out_str3 += '|_| ';		  
 	  }else if (str[i] == 0){
-		out_str3 += '|_|';
+		out_str3 += '|_| ';
 	  }else if (str[i] == 2){
-		out_str3 += '|_.';
+		out_str3 += '|_. ';
 	  }else if (str[i] == 3){
-		out_str3 += '._|';
+		out_str3 += '._| ';
 	  }else if (str[i] == 5){
-		out_str3 += '._|';
+		out_str3 += '._| ';
 	  }else {
-		out_str3 += '..|';
+		out_str3 += '..| ';
 	  }
 	}
 	out_str = out_str1 + out_str2 + out_str3
